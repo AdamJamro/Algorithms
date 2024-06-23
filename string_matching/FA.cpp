9076ff58579ca2e85156e6c1841e63a7314ff999
AdamDzem <115588127+AdamJamro@users.noreply.github.com>
2024-06-23 10:30:22 +0200

    Update FA.cpp

--- string_matching/FA.cpp
+++ string_matching/FA.cpp
@@ -5,18 +5,17 @@
 using std::string;
 using std::vector;
 
-vector<unsigned int> finite_automaton_matcher(const string&, const string&, unsigned int);
-vector<unsigned int> finite_automaton_matcher(const string&, const string&, unsigned int, unsigned int*);
-unsigned int* compute_transition_function(const string&, unsigned int);
+vector<unsigned int> finite_automaton_matcher(const string&, const string&, size_t);
+vector<unsigned int> finite_automaton_matcher(const string&, const string&, size_t, const unsigned int*);
+unsigned int* compute_transition_function(const string&, size_t);
 
-inline size_t make_2d(size_t a, size_t b, unsigned int alphabet_size=256) {
+inline size_t make_2d(size_t a, size_t b, size_t alphabet_size=256) {
     return a * alphabet_size + b;
 }
 
 // function allocates memory and never deletes it
-unsigned int* compute_transition_function(const string& pattern, unsigned int alphabet_size=256) {
+unsigned int* compute_transition_function(const string& pattern, const size_t alphabet_size=256) {
     const size_t pattern_len = pattern.length();
-
 
     auto* prefix = new unsigned int[pattern_len];
     prefix[0] = 0;
@@ -37,10 +36,10 @@ unsigned int* compute_transition_function(const string& pattern, unsigned int al
 
     for (int letter = 0; letter < alphabet_size; letter++) {
         if (letter == pattern[0]) {
-            transition_fun[make_2d(0, letter)] = 1;
+            transition_fun[make_2d(0, letter, alphabet_size)] = 1;
         }
         else {
-            transition_fun[make_2d(0, letter)] = 0;
+            transition_fun[make_2d(0, letter, alphabet_size)] = 0;
         }
         for (int i = 1; i <= pattern_len; i++) {
             const int& state = i;
@@ -57,7 +56,7 @@ unsigned int* compute_transition_function(const string& pattern, unsigned int al
 
             // from current state with current letter we go into ith state
             // (we found input postfix of length i that matches ith prefix of pattern)
-            auto& next_state = transition_fun[make_2d(state, letter)];
+            auto& next_state = transition_fun[make_2d(state, letter, alphabet_size)];
             if (pattern_len == state and letters_matched == pattern_len) {
                 next_state = 0;
             }
@@ -71,7 +70,7 @@ unsigned int* compute_transition_function(const string& pattern, unsigned int al
     return transition_fun;
 }
 
-vector<unsigned int> finite_automaton_matcher(const string& text, const string& pattern) {
+vector<unsigned int> finite_automaton_matcher(const string& text, const string& pattern, const size_t alphabet_size=256) {
     const size_t text_len = text.length();
     const size_t pattern_len = pattern.length();
 
@@ -86,14 +85,15 @@ vector<unsigned int> finite_automaton_matcher(const string& text, const string& 
     }
     offsets.reserve(int(text_len / pattern_len));
 
+
     // creating the automaton
-    auto* transition_fun = compute_transition_function(pattern);
+    auto* transition_fun = compute_transition_function(pattern, alphabet_size);
 
 
     // simulation
     unsigned int q = 0;
     for (int i = 0; i < text_len; i++) {
-        q = transition_fun[make_2d(q, text[i])];
+        q = transition_fun[make_2d(q, text[i], alphabet_size)];
 
         if (q == pattern_len) {
             offsets.push_back(i - pattern_len + 1);
@@ -108,7 +108,7 @@ vector<unsigned int> finite_automaton_matcher(const string& text, const string& 
     return offsets;
 }
 
-vector<unsigned int> finite_automaton_matcher(const string& text, const string& pattern, const unsigned int* transition_fun) {
+vector<unsigned int> finite_automaton_matcher(const string& text, const string& pattern, const size_t alphabet_size, const unsigned int* transition_fun) {
     const size_t text_len = text.length();
     const size_t pattern_len = pattern.length();
 
@@ -126,7 +126,7 @@ vector<unsigned int> finite_automaton_matcher(const string& text, const string& 
     // simulation
     unsigned int q = 0;
     for (int i = 0; i < text_len; i++) {
-        q = transition_fun[make_2d(q, text[i])];
+        q = transition_fun[make_2d(q, text[i], alphabet_size)];
 
         if (q == pattern_len) {
             offsets.push_back(i - pattern_len + 1);
@@ -153,6 +153,7 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    size_t alphabet_size = 256;
     // precompute the automaton
     auto* transition_function = compute_transition_function(pattern, 256);
 
@@ -165,7 +166,7 @@ int main(int argc, char *argv[]) {
         while ( getline (file_stream, line) )
         {
             line_count++;
-            vector<unsigned int> found_matches = finite_automaton_matcher(line, pattern, transition_function);
+            vector<unsigned int> found_matches = finite_automaton_matcher(line, pattern, alphabet_size, transition_function);
 
             if (found_matches[0] != -1) {
                 std::cout << "Line " << line_count << " at: ";
